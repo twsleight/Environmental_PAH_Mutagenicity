808f4bf0cf8da549f178bb18fdda1017252f926b
twsleight <twsleight@gmail.com>
2020-11-13 22:14:13 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Environmental_PAH_Mutagenicity',
+   version='1.0',
+   description='small hydrocarbon QSAR for direct acting Ames test mutagens',
+   author='Trevor Sleight',
+   author_email='twsleight@gmail.com',
+   packages=['Environmental_PAH_Mutagenicity'],  #same as name
+   install_requires=['numpy~=1.19', 'pandas>=1.1'], #external packages as dependencies
+)
