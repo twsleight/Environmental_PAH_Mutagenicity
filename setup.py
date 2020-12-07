9e32492ad3ce8adfeff0647f54d8c2a050888014
twsleight <twsleight@gmail.com>
2020-12-06 21:59:52 -0500

    adding requirments to setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(
    author='Trevor Sleight',
    author_email='twsleight@gmail.com',
    packages=['Environmental_PAH_Mutagenicity'],  #same as name
-   install_requires=['numpy~=1.19', 'pandas>=1.1'], #external packages as dependencies
+   install_requires=['numpy~=1.19', 
+                     'pandas>=1.1', 
+                     'scikit-learn>=0.23.2',
+                     'statsmodels==0.12.0'], #external packages as dependencies
 )
