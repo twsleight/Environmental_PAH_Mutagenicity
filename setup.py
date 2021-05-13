1dcb06e4f9d32a81f7c85ec7e7ffb98944551086
twsleight <twsleight@gmail.com>
2021-05-12 20:27:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,14 @@ setup(
    author='Trevor Sleight',
    author_email='twsleight@gmail.com',
    packages=['Environmental_PAH_Mutagenicity'],  #same as name
-   install_requires=['numpy~=1.19', 'pandas>=1.1'], #external packages as dependencies
+   install_requires=['numpy~=1.19',
+                     'pandas>=1.1',
+                     'xlsxwriter~=1.3',
+                     'scikit-learn~=0.23',
+                     'statsmodels~=0.12',
+                     'openbabel~=3.1',
+                     'openbabel-python>=1.6',
+                     'padelpy~=0.1', 
+                     
+                     ], #external packages as dependencies
 )
