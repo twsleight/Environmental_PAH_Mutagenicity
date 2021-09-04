c104b4a98455e0e3763966429ea1596886e8c6d3
twsleight <twsleight@gmail.com>
2021-09-04 12:36:35 -0400

    Update SI_swam_plots.py

--- plots/SI_swam_plots.py
+++ plots/SI_swam_plots.py
@@ -1,5 +1,5 @@
-from IPython import get_ipython;   
-get_ipython().magic('reset -sf')
+# from IPython import get_ipython;   
+# get_ipython().magic('reset -sf')
 
 import seaborn as sns
 import pandas as pd
@@ -24,17 +24,27 @@ clust_DF = pd.read_excel(filename, sheet_name = 'cluster_assignments')
 
 
 
-# 0 is the small cluster of 215 molecules
-# 2 is the large molecules cluster of 301 molecules
+# 0 is the small cluster of 240 molecules
+# 2 is the large molecules cluster of 315 molecules
 
 #use to select data from just one cluster
-data = data.loc[clust_DF['cluster']==0]
+data = data.loc[clust_DF['cluster']==2]
 
 #set up the plotting parameters
 
-plot_Data = pd.DataFrame(data = {'RDF55s':data['RDF55s'],
-                                 'HOMO-LUMO Gap (eV)' : data['HLgap'],                                 
-                                 'Mutagenicity':data['result'] } )
+# plot_Data = pd.DataFrame(data = {'HOMO':data['HOMO'],
+#                                  'HOMO' : data['HOMO']*27.2114,                                 
+#                                  'Mutagenicity':data['result'] } )
+
+plot_Data = pd.DataFrame(data = {'SpMin5_Bhi':data['SpMin5_Bhi'],
+                                  'SpMin5_Bhi' : data['SpMin5_Bhi'],                                 
+                                  'Mutagenicity':data['result'] } )
+
+# plot_Data = pd.DataFrame(data = {'RDF55s':data['RDF55s'],
+#                                   'RDF55s' : data['RDF55s']*27.2114,                                 
+#                                   'Mutagenicity':data['result'] } )
+
+
 
 plot_Data.replace({1: 'Positive' , 0:'Negative' }, inplace = True)
 
@@ -42,14 +52,14 @@ plot_Data.replace({1: 'Positive' , 0:'Negative' }, inplace = True)
 fig = plt.figure(figsize = (10,8))
 ax1 = fig.add_subplot(1, 1, 1)
 
-sns.swarmplot(x="Mutagenicity", y="RDF55s", data=plot_Data, ax = ax1)
+sns.swarmplot(x="Mutagenicity", y="SpMin5_Bhi", data=plot_Data, ax = ax1)
 
 sns.set_theme(style="whitegrid")
 sns.despine()
 
 # plt.ylabel("HOMO-LUMO Gap (eV)", fontsize = 18, labelpad = 15)
 
-plt.ylabel("RDF55s", fontsize = 18, labelpad = 8)
+plt.ylabel("SpMin5_Bhi", fontsize = 18, labelpad = 8)
 plt.xlabel("Mutagenicity", fontsize = 18, labelpad = 15)
 
 
@@ -57,4 +67,4 @@ plt.yticks(fontsize=15)
 plt.xticks(fontsize=15) 
 
 
-# fig.savefig(r'RDF55s_swam_all.jpg', format='jpg', dpi=1200)
+# fig.savefig(r'SpMin5_Bhi_swam_all.jpg', format='jpg', dpi=1200)
